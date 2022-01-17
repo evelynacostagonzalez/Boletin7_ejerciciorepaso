d2040b1c4fc16bdc91efa32932cab1f1866a8b90
Evelyn <evelyn.acosta@iesnervion.es>
2022-01-17 13:58:28 +0100

    1

---
+++ src/com/company/Cuenta1.java
@@ -0,0 +1,90 @@
+package com.company;
+
+public class Cuenta1 {
+
+        private String nombreCliente, numeroCuenta;
+        private double tipoInteres;
+        private double saldo;
+
+
+        public Cuenta1() {
+
+        }
+
+        public Cuenta1(String nombreCliente, String numeroCuenta, double tipoInteres, double saldo) {
+
+
+            this.nombreCliente = nombreCliente;
+            this.numeroCuenta = numeroCuenta;
+            this.tipoInteres = tipoInteres;
+            this.saldo = saldo;
+        }
+
+        //getter y setter
+        public String getNumeroCuenta() {
+            return numeroCuenta;
+        }
+
+        public double gettipoInteres() {
+            return tipoInteres;
+        }
+
+        public String getnombreCliente() {
+            return nombreCliente;
+        }
+
+        public double getsaldo() {
+            return saldo;
+        }
+
+        public void setNombreCliente(String nombreCliente) {
+            this.nombreCliente = nombreCliente;
+        }
+
+        public void setNumeroCuenta(String numeroCuenta) {
+            this.numeroCuenta = numeroCuenta;
+        }
+
+        public void setTipoInteres(double tipoInteres) {
+            this.tipoInteres = tipoInteres;
+        }
+        public void setSaldo (double saldo){
+            this.saldo = saldo;
+        }
+
+        public Cuenta1 (com.company.Cuenta cuenta) {
+            this.nombreCliente = cuenta.getnombreCliente();
+            this.numeroCuenta = cuenta.getNumeroCuenta();
+            this.tipoInteres = cuenta.gettipoInteres();
+            this.saldo = cuenta.getsaldo();
+        }
+
+        public boolean ingresar(double cantidad) {
+            boolean realizado = false;
+            if (cantidad > 0) {
+                this.saldo += cantidad;
+                realizado = true;
+            }
+            return realizado;
+
+            public boolean reintegro (double cantidad){
+                boolean realizado = false;
+                if (this.getsaldo() >= cantidad && cantidad > 0) {
+                    this.saldo -= cantidad;
+                    realizado = true;
+                }
+                return realizado;
+
+            }
+
+            public boolean transferencia (Cuenta cuentaDestino, double cantidad) {
+                boolean realizado = false;
+                if (cantidad <= this.getsaldo() && cantidad >0){
+
+                }
+            }
+
+        }
+    }
+
+}
---
+++ src/cubo/Main.java
@@ -0,0 +1,13 @@
+package cubo;
+
+public class Main {
+    public static void main (String[] args){
+        Cuadrado cuadrado1 = new Cuadrado(4);
+        cuadrado1.toString();
+        System.out.println(cuadrado1);
+    }
+
+
+
+
+}
